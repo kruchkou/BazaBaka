2a4e5f4dc30e040cb27a2bce39f72e162bd88352
zhigarevyan <zhigarevyan@users.noreply.github.com>
2020-05-05 03:25:42 +0300

    кек

--- PHP/start.php
+++ PHP/start.php
@@ -39,14 +39,31 @@ if (($json = valid()) == false) { echo "Hi, bax_100 =)"; exit(); }
 		case 'Матчи двух игроков':
 	  $players = getPlayers();
 	  $ANSWER = "";
-	  
+
 	  foreach ($players as $row) {
 	     $id = $row['idplayers'];
 	     $name =$row['name'];
 	     $ANSWER.=strval($id)." | ".strval($name)."\n";
 	  }
+  //   $result = getTwoPlayersMatches("Андрей Факеев","Михаил Будников")
+  // $ANSWER = "";
+  // $counter = 0;
+  // foreach($result as $row) {
+  // $counter++;
+  // $player1 = $row['player1'];
+  // $player2 = $row['player2'];
+  // $sets = $row['result'];
+  // $date = $row['date'];
+  // $ANSWER .= strval($player1)." ".strval($player2)." ".strval($sets)." ".$date."\n";
+  //}
 		//$ANSWER = "Захотееел, захотееел";
 		break;
+    // case 'Матчи двух игроков':
+
+    // }
+    // break;
+
+
 
 		case 'Матчи игрока':
 		//тут вызов функции
@@ -56,7 +73,7 @@ if (($json = valid()) == false) { echo "Hi, bax_100 =)"; exit(); }
 		case 'Последние матчи':
 		//тут вызов функции
 		//$ANSWER = "Захотееел, захотееел";
-    $result = getLastMatches();
+    $result = getLastMatches1();
     $ANSWER = "";
     $counter = 0;
     foreach($result as $row) {
@@ -68,6 +85,7 @@ if (($json = valid()) == false) { echo "Hi, bax_100 =)"; exit(); }
     $ANSWER .= strval($player1)." ".strval($player2)." ".strval($sets)." ".$date."\n";
     }
     break;
+
 
 case '/start':
 $ANSWER = "Чего хочешь?";
